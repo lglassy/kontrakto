40ea168af294d66deeaae917dd518f019686e9b8
lou <lou@chromebook.example.org>
2017-12-30 18:55:05 -0500

    add examples of using contract decorators on methods

--- external_test.py
+++ external_test.py
@@ -1,5 +1,5 @@
-# this file is a simple example of how you would use 
-# kontrakto from normal client code.
+# this file contains simple examples of how you would use the kontrakto
+# module with normal client code.
 
 import kontrakto 
 
@@ -14,7 +14,7 @@ from kontrakto import pre, post, post_arg
 
 kontrakto.CONTRACTS_ENABLED = True 
 
-def run_tests():
+def run_tests_on_decorated_functions():
 
     @pre(lambda a, b: a > 0 and b > 0, 'a and b must be positive')
     def target(a, b):
@@ -93,9 +93,55 @@ def run_tests():
         print('multi, expect exception:', e)
         print('-')
 
+class Counter(object):
+    # note that when we decorate a method, we must include 
+    # the class instance itself as an arg to the contract function.
+
+    def __init__(self, val):
+        self.val = val 
+
+    @pre(lambda self: self.val > 0, 'counter val must before inc must be greater than zero')
+    def inc(self):
+        self.val += 1
+
+    @pre(lambda self, ival: (self.val + ival) % 2 == 0,
+          'new val after incby must be even')
+    def incby(self, ival):
+        self.val += ival
+    
+def run_tests_on_decorated_methods():
+    C = Counter(1)
+    print('counter C val before inc', C.val)
+    C.inc()
+    print('counter C val after inc ', C.val)
+    print()
+
+    try:
+        C2 = Counter(0)
+        print('counter C2 val before inc', C2.val)
+        C2.inc()
+        print('counter C2 val after inc ', C2.val)
+    except kontrakto.Contract_Error as e:
+        print('expect exception:', e)
+    print()
+
+    C3 = Counter(1)
+    print('counter C3 val', C3.val)
+    C3.inc()
+    print('counter C3 val after inc', C3.val)
+    C3.incby(2)
+    print('counter C3 val after incby', C3.val)
+    try:
+        C3.incby(3)
+        print('counter C3 val after 2nd incby', C3.val)
+    except kontrakto.Contract_Error as e:
+        print('expect exception:', e)
+    print()
+
 # - - - 
 
 if __name__ == '__main__':
-    run_tests()
+    run_tests_on_decorated_functions()
+    run_tests_on_decorated_methods()
 
 # end of file
